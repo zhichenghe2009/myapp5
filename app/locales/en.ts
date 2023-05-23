a3decf8bb9265e1a5b3ff3246cd0800b2e85d84a
zhichenghe2008 <zhichenghe@deeproute.ai>
2023-05-23 17:01:39 +0800

    test

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -5,7 +5,7 @@ const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized:
-    "未授权，请找到设置按钮配置临时授权码:123456,或者填入你自己的API Key独享使用，若没找到配置按钮可参考： (验证码或api-key手机端设置指引 https://docs.qq.com/doc/DZElpcG9lWERGd1dw?u=355c394f2acc42e4bec073093f2b96f8)   (验证码或api-key电脑端设置指引 https://docs.qq.com/doc/DZFRWeXNaU25Wc2RM?u=355c394f2acc42e4bec073093f2b96f8)     还请及时加入到qq服务群29898932或者访问 https://www.chatgptvip.shop 扫码加群，虽然大家现在很少使用qq，但是加群后即可不再理会，万一站点被墙或无法访问时即可到qq群中获取新的访问点，访问此站点遇到任何问题也可在qq群中反馈",
+    "未授权，请找到设置按钮配置临时授权码:123456,或者填入你自己的API Key独享使用，若没找到配置按钮可参考： (验证码或api-key手机端设置指引 https://docs.qq.com/doc/DZElpcG9lWERGd1dw?u=355c394f2acc42e4bec073093f2b96f8)   (验证码或api-key电脑端设置指引 https://docs.qq.com/doc/DZFRWeXNaU25Wc2RM?u=355c394f2acc42e4bec073093f2b96f8)      还请及时加入到qq服务群29898932或者访问 https://www.chatgptvip.shop 扫码加群，虽然大家很少用qq，但加群后即可不再理会，万一站点被墙或无法访问时即可到qq群中获取新的访问点，访问站点遇到任何问题也可在qq群中反馈",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} messages`,
