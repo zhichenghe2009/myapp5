bd5a081e74e526b6613d19f70a1c2fdd1413414a
zhichenghe2008 <zhichenghe@deeproute.ai>
2023-05-23 14:12:58 +0800

    test

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -5,7 +5,7 @@ const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized:
-    "未授权，请找到设置按钮配置临时授权码:123456,或者填入你自己的API Key独享使用，若没找到可参考： 验证码或api-key手机端设置指引 https://docs.qq.com/doc/DZElpcG9lWERGd1dw?u=355c394f2acc42e4bec073093f2b96f8   验证码或api-key电脑端设置指引 https://docs.qq.com/doc/DZFRWeXNaU25Wc2RM?u=355c394f2acc42e4bec073093f2b96f8",
+    "未授权，请找到设置按钮配置临时授权码:123456,或者填入你自己的API Key独享使用，若没找到配置按钮可参考： (验证码或api-key手机端设置指引 https://docs.qq.com/doc/DZElpcG9lWERGd1dw?u=355c394f2acc42e4bec073093f2b96f8)   (验证码或api-key电脑端设置指引 https://docs.qq.com/doc/DZFRWeXNaU25Wc2RM?u=355c394f2acc42e4bec073093f2b96f8)",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} messages`,
