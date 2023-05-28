5acd4625cf5e6726402eee757e6028b198d9a764
zhichenghe2008 <zhichenghe@deeproute.ai>
2023-05-28 20:21:56 +0800

    test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -147,17 +147,17 @@ export function SideBar(props: { className?: string }) {
       </div>
 
       <div className={styles["sidebar-bar-button"]}>
-        <a href="https://chatgptvip.shop/ai%e5%b7%a5%e5%85%b7%e9%83%a8%e7%bd%b2%e4%bf%9d%e5%a7%86%e7%ba%a7%e6%95%99%e7%a8%8b/" target="_blank">
+        <a href="https://chatgptvip.shop/product/chatgptpass/" target="_blank">
           <IconButton
             icon={<GithubIcon />}
-            text='Ai工具部署保姆级指导书'
+            text='访问密码购买'
             shadow
           />
         </a>
       </div>
 
       <div className={styles["sidebar-bar-button"]}>
-        <a href="https://www.chatgptvip.shop/chatgpt%e4%b8%ad%e6%96%87%e8%b0%83%e6%95%99%e6%8c%87%e5%8d%97" target="_blank">
+        <a href="https://chatgptvip.shop/chatgpt%e4%b8%ad%e6%96%87%e8%b0%83%e6%95%99%e6%8c%87%e5%8d%97/" target="_blank">
           <IconButton
             icon={<GithubIcon />}
             text='ChatGpt中文调教指南'
@@ -171,6 +171,16 @@ export function SideBar(props: { className?: string }) {
           <IconButton
             icon={<GithubIcon />}
             text='ChatGpt私有账号购买'
+            shadow
+          />
+        </a>
+      </div>
+
+      <div className={styles["sidebar-bar-button"]}>
+        <a href="https://docs.qq.com/doc/DZHJQam5PZG5JelRO?u=355c394f2acc42e4bec073093f2b96f8" target="_blank">
+          <IconButton
+            icon={<GithubIcon />}
+            text='微信使用Midiourney绘图'
             shadow
           />
         </a>
